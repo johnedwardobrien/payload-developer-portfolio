2ce3f32d8cb2f703cfc4d46c7137f4f7f341f056
John O'Brien <johnobrien8642@gmail.com>
2025-10-31 19:00:50 -0700

    comment out canUseDOM, add johnobrien.dev to remotePatterns

--- src/utilities/getURL.ts
+++ src/utilities/getURL.ts
@@ -10,17 +10,17 @@ export const getServerSideURL = () => {
 }
 
 export const getClientSideURL = () => {
-  if (canUseDOM) {
-    const protocol = window.location.protocol
-    const domain = window.location.hostname
-    const port = window.location.port
+  // if (canUseDOM) {
+  //   const protocol = window.location.protocol
+  //   const domain = window.location.hostname
+  //   const port = window.location.port
 
-    return `${protocol}//${domain}${port ? `:${port}` : ''}`
-  }
+  //   return `${protocol}//${domain}${port ? `:${port}` : ''}`
+  // }
 
   if (process.env.VERCEL_PROJECT_PRODUCTION_URL) {
     return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
   }
-
+  
   return process.env.NEXT_PUBLIC_SERVER_URL || ''
 }
