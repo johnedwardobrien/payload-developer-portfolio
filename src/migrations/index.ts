768dd2710238d2dca828d23c4e9f374c998149eb
John O'Brien <johnobrien8642@gmail.com>
2025-11-15 00:18:42 -0800

    try removing migrations again

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20251115_080551 from './20251115_080551';
-
-export const migrations = [
-  {
-    up: migration_20251115_080551.up,
-    down: migration_20251115_080551.down,
-    name: '20251115_080551'
-  },
-];
