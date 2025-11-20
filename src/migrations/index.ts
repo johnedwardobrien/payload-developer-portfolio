78d45186ccddad80261f26e31fd86c7985b5ae77
John O'Brien <johnobrien8642@gmail.com>
2025-11-19 21:06:16 -0800

    add blank migration file to reset payload migrate create

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20251115_094900_add_video_thumbnail_fields from './20251115_094900_add_video_thumbnail_fields';
 import * as migration_20251115_094900_add_video_thumbnail_fields_2 from './20251115_094900_add_video_thumbnail_fields_2';
+import * as migration_20251120_050201 from './20251120_050201';
 
 export const migrations = [
   {
@@ -11,5 +12,10 @@ export const migrations = [
     up: migration_20251115_094900_add_video_thumbnail_fields_2.up,
     down: migration_20251115_094900_add_video_thumbnail_fields_2.down,
     name: '20251115_094900_add_video_thumbnail_fields_2',
+  },
+  {
+    up: migration_20251120_050201.up,
+    down: migration_20251120_050201.down,
+    name: '20251120_050201'
   },
 ];
