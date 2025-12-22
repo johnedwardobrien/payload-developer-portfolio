f1ec1b12934b2ec3fdd7475b1cefe8ac2a6758d3
John O'Brien <johnobrien8642@gmail.com>
2025-12-21 19:21:22 -0800

    remove migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,26 +1,25 @@
-import * as migration_20251115_094900_add_video_thumbnail_fields from './20251115_094900_add_video_thumbnail_fields';
-import * as migration_20251115_094900_add_video_thumbnail_fields_2 from './20251115_094900_add_video_thumbnail_fields_2';
-import * as migration_20251120_050201 from './20251120_050201';
-import * as migration_20251120_050631 from './20251120_050631';
-import * as migration_20251121_020431 from './20251121_020431';
-import * as migration_20251220_041204 from './20251220_041204';
-import * as migration_20251220_194412 from './20251220_194412';
-import * as migration_20251220_212151 from './20251220_212151';
-import * as migration_20251221_010216 from './20251221_010216';
-import * as migration_20251221_033029 from './20251221_033029';
-import * as migration_20251221_033320 from './20251221_033320';
-import * as migration_20251221_033418 from './20251221_033418';
-import * as migration_20251221_194155 from './20251221_194155';
-import * as migration_20251221_214000 from './20251221_214000';
-import * as migration_20251221_215525 from './20251221_215525';
-import * as migration_20251222_000235 from './20251222_000235';
-import * as migration_20251222_020129 from './20251222_020129';
-import * as migration_20251222_020647 from './20251222_020647';
-import * as migration_20251222_022254 from './20251222_022254';
-import * as migration_20251222_023408 from './20251222_023408';
-import * as migration_20251222_024227 from './20251222_024227';
-import * as migration_20251222_025304 from './20251222_025304';
-import * as migration_20251222_031705 from './20251222_031705';
+import * as migration_20251115_094900_add_video_thumbnail_fields from './20251115_094900_add_video_thumbnail_fields'
+import * as migration_20251115_094900_add_video_thumbnail_fields_2 from './20251115_094900_add_video_thumbnail_fields_2'
+import * as migration_20251120_050201 from './20251120_050201'
+import * as migration_20251120_050631 from './20251120_050631'
+import * as migration_20251121_020431 from './20251121_020431'
+import * as migration_20251220_041204 from './20251220_041204'
+import * as migration_20251220_194412 from './20251220_194412'
+import * as migration_20251220_212151 from './20251220_212151'
+import * as migration_20251221_010216 from './20251221_010216'
+import * as migration_20251221_033029 from './20251221_033029'
+import * as migration_20251221_033320 from './20251221_033320'
+import * as migration_20251221_033418 from './20251221_033418'
+import * as migration_20251221_194155 from './20251221_194155'
+import * as migration_20251221_214000 from './20251221_214000'
+import * as migration_20251221_215525 from './20251221_215525'
+import * as migration_20251222_000235 from './20251222_000235'
+import * as migration_20251222_020129 from './20251222_020129'
+import * as migration_20251222_020647 from './20251222_020647'
+import * as migration_20251222_022254 from './20251222_022254'
+import * as migration_20251222_023408 from './20251222_023408'
+import * as migration_20251222_024227 from './20251222_024227'
+import * as migration_20251222_025304 from './20251222_025304'
 
 export const migrations = [
   {
@@ -133,9 +132,4 @@ export const migrations = [
     down: migration_20251222_025304.down,
     name: '20251222_025304',
   },
-  {
-    up: migration_20251222_031705.up,
-    down: migration_20251222_031705.down,
-    name: '20251222_031705'
-  },
-];
+]
