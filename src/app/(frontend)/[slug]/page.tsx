416881900bd499f145ed6449818c85a36453f266
John O'Brien <johnobrien8642@gmail.com>
2026-01-06 18:57:29 -0800

    update depth for population

--- src/app/(frontend)/[slug]/page.tsx
+++ src/app/(frontend)/[slug]/page.tsx
@@ -99,6 +99,7 @@ const queryPageBySlug = cache(async ({ slug }: { slug: string }) => {
     limit: 1,
     pagination: false,
     overrideAccess: draft,
+    depth: 10,
     where: {
       slug: {
         equals: slug,
