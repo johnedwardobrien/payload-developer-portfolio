e6279e053387fac1981cbe1ff92239382d743ec0
John O'Brien <johnobrien8642@gmail.com>
2026-02-03 14:19:31 -0800

    remove default payload meta titles and images

--- src/utilities/generateMeta.ts
+++ src/utilities/generateMeta.ts
@@ -7,15 +7,11 @@ import { getServerSideURL } from './getURL'
 
 const getImageURL = (image?: Media | Config['db']['defaultIDType'] | null) => {
   const serverUrl = getServerSideURL()
-
-  let url = serverUrl + '/website-template-OG.webp'
-
+  let url = ''
   if (image && typeof image === 'object' && 'url' in image) {
     const ogUrl = image.sizes?.og?.url
-
     url = ogUrl ? serverUrl + ogUrl : serverUrl + image.url
   }
-
   return url
 }
 
@@ -26,9 +22,7 @@ export const generateMeta = async (args: {
 
   const ogImage = getImageURL(doc?.meta?.image)
 
-  const title = doc?.meta?.title
-    ? doc?.meta?.title + ' | Payload Website Template'
-    : 'Payload Website Template'
+  const title = doc?.meta?.title ?? ''
 
   return {
     description: doc?.meta?.description,
