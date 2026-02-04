ff7e204009f5b1031b593d53ab2e12393cdfe762
John O'Brien <johnobrien8642@gmail.com>
2026-02-04 00:12:40 -0800

    fix image meta

--- src/utilities/generateMeta.ts
+++ src/utilities/generateMeta.ts
@@ -8,9 +8,10 @@ import { getServerSideURL } from './getURL'
 const getImageURL = (image?: Media | Config['db']['defaultIDType'] | null) => {
   const serverUrl = getServerSideURL()
   let url = ''
+
   if (image && typeof image === 'object' && 'url' in image) {
     const ogUrl = image.sizes?.og?.url
-    url = ogUrl ? serverUrl + ogUrl : serverUrl + image.url
+    url = ogUrl ? ogUrl : serverUrl + image.url
   }
   return url
 }
